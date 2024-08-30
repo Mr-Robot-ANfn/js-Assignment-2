64556d319a6b643c65f7219e31e265bc503abc77
Shahab-Saeed <161280552+Shahab-Saeed@users.noreply.github.com>
2024-08-30 11:54:57 +0500

    Add files via upload

---
+++ sr2.js
@@ -0,0 +1,48 @@
+// QUESTION 1
+
+// var username;
+
+// QUESTION 2
+
+// var myName="Shahab Saeed";
+
+
+// QUESTION 3
+
+// var message;
+// message="Hello World";
+// alert(message);
+
+
+// QUESTION 4
+
+// var name="Shahab Saeed"
+// var age=21;
+// var course="Web And Mobile App Development";
+
+// alert(name);
+// alert(age);
+// alert(course);
+
+// QUESTION 5
+
+// var s="PIZZA";
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+
+// QUESTION 6
+
+// var email;
+// email="falana@gmail.com"
+// alert("My email address is: "+email);
+
+
+// QUESTION 7
+
+// var book;
+// book="A smarter way to learn javascript";
+// alert("i am trying learn from the Book "+book);
+
+// QUESTION 8
+
+// document.write("Yah! I can write HTML content through Java Script")
